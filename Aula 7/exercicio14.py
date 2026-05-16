04d94a529fd33ba7b0c6fa96ca92c914628fdb49
Toni Silva <baianodeposito9@gmail.com>
2026-05-16 12:37:58 -0300

    Terminados os exercícios 14 e 15

---
+++ Aula 7/exercicio14.py
@@ -0,0 +1,2 @@
+temp = int(input("Digite a temperatura"))
+print(f" a temperatura é: {((temp * 9) / 5) + 32}")
---
+++ Aula 7/exercicio15.py
@@ -0,0 +1,3 @@
+diasalugados = int(input("Quantos dias alugados?"))
+kmsrodados = int(input("Quantos kms rodados?"))
+print(f" o total a pagar é: {(0.15 * kmsrodados) + (diasalugados * 60)}")
