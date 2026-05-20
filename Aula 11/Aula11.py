53bc449c3427a3c6e7636d3b35868b18c5bce819
Toni Silva <baianodeposito9@gmail.com>
2026-05-20 09:13:52 -0300

    Create Aula11.py

---
+++ Aula 11/Aula11.py
@@ -0,0 +1,12 @@
+
+cores = {
+    'vermelho': '\033[31m',
+    'azul': '\033[34m',
+    'roxo': '\033[35m'
+}
+
+print('\033[7;33;44mOlá, mundo!\033[m')
+
+print("""\033[1;35mTeste 1\033[m""") 
+
+print(f"Eu ainda amo aquela garota dos {cores['vermelho']}cachos vermelhos{'\033[m'} ")
