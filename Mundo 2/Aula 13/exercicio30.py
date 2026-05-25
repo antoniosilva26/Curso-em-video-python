36eda5a562a8b9e41d6c993942ed6459afb683dc
Toni Silva <baianodeposito9@gmail.com>
2026-05-25 00:14:24 -0300

    Update exercicio30.py

--- Mundo 2/Aula 13/exercicio30.py
+++ Mundo 2/Aula 13/exercicio30.py
@@ -1,6 +1,11 @@
-palavra = str(input("Digite umaaaaaa palavra!"))
-print(palavra.replace(" ", ""))
+frase = str(input("Digite uma frase: ")).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+if inverso == junto:
+    print("Temos um palíndromo!")
+else:
+    print("a frase digitada não é um palindromo!")
 
-
-
-aaaaaaaaaaaaaaaaaaaaaaaaaa
