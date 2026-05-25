51e77572d57e84159267c24c6c405e270f398bd4
Toni Silva <baianodeposito9@gmail.com>
2026-05-24 23:59:36 -0300

    Update exercicio30.py

--- Mundo 2/Aula 13/exercicio30.py
+++ Mundo 2/Aula 13/exercicio30.py
@@ -1,7 +1,5 @@
 palavra = str(input("Digite uma palavra!"))
-quantpala = len(palavra.replace(" ", ""))
-print(quantpala)
+print(palavra.replace(" ", ""))
 
 
-for c in range(quantpala, 0, -1):
-    print(palavra.find(quantpala))
+
